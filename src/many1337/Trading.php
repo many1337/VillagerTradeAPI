e494a05824ba2c01c0b501ea330b1ef7ba11fffa
many1337 <31310430+many1337@users.noreply.github.com>
2020-02-20 12:22:21 +0100

    this is also fixed lmao

--- src/many1337/Trading.php
+++ src/many1337/Trading.php
@@ -10,99 +10,80 @@ use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\item\Item;
 use pocketmine\math\Vector3;
-use pocketmine\nbt\NBT;
 use pocketmine\nbt\tag\ByteTag;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\nbt\tag\IntTag;
 use pocketmine\nbt\tag\ListTag;
-use pocketmine\nbt\tag\ShortTag;
-use pocketmine\network\mcpe\protocol\AddEntityPacket;
+use pocketmine\nbt\NetworkLittleEndianNBTStream;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\network\mcpe\protocol\AddActorPacket;
 use pocketmine\network\mcpe\protocol\ContainerClosePacket;
-use pocketmine\network\mcpe\protocol\EntityEventPacket;
+use pocketmine\network\mcpe\protocol\ActorEventPacket as EntityEventPacket;
 use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;
-use pocketmine\network\mcpe\protocol\RemoveEntityPacket;
+use pocketmine\network\mcpe\protocol\RemoveActorPacket as RemoveEntityPacket;
+use pocketmine\network\mcpe\protocol\types\WindowTypes;
 use pocketmine\network\mcpe\protocol\UpdateTradePacket;
 use pocketmine\plugin\PluginBase;
 
 class Trading extends PluginBase implements Listener{
 	/** @var array */
 	public $villagerId = [];
-	/** @var array */
-	public $recipes = [];
+
+	/** @var Entity */
+	public $en;
 
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->getServer()->getLogger()->notice("Please Wait... Repcies makeing♡");
 	}
 
-	public function makeRecipe(Item $buyA, Item $sell, Item $buyB = null){
-		if($buyB === null){
-			return new CompoundTag("", [
-				$buyA->nbtSerialize(-1, "buyA"),
-				new IntTag("maxUses", 32767),
-				new ByteTag("rewardExp", 0),
-				$sell->nbtSerialize(-1, "sell"),
-				new IntTag("uses", 0),
-			]);
-		}else{
-			return new CompoundTag("", [
-				$buyA->nbtSerialize(-1, "buyA"),
-				$buyB->nbtSerialize(-1, "buyB"),
-				new IntTag("maxUses", 32767),
-				new ByteTag("rewardExp", 0),
-				$sell->nbtSerialize(-1, "sell"),
-				new IntTag("uses", 0),
-			]);
-		}
-	}
-
 	public function onClick(PlayerInteractEvent $e){
 		$p = $e->getPlayer();
 		if($e->getAction() === PlayerInteractEvent::RIGHT_CLICK_BLOCK){
 			$this->villagerId[$p->getName()] = $eid = mt_rand(0xfffff, 0x7fffffff);
-			$pk = new AddEntityPacket();
+			$pk = new AddActorPacket();
 			$pk->entityRuntimeId = $eid;
 			$pk->type = Villager::NETWORK_ID;
-			$pk->position = $p->subtract(0, 4, 0);
-			$pk->motion = new Vector3;
+			$pk->position = new Vector3();
+			$pk->motion = new Vector3();
 			$pk->yaw = $pk->pitch = 0;
 			$pk->metadata = [
 				Entity::DATA_FLAGS => [Entity::DATA_TYPE_LONG, 1 << Entity::DATA_FLAG_IMMOBILE],
 				Entity::DATA_SCALE => [Entity::DATA_TYPE_FLOAT, 0]
 			];
 			$p->dataPacket($pk);
-
-			$tag = new CompoundTag("", [
-				new ListTag("Recipes", []),
+			$pk = new UpdateTradePacket();
+			$pk->windowId = WindowTypes::TRADING;
+			$pk->tradeTier = 1;
+			$pk->isWilling = false;
+			$pk->traderEid = $eid;
+			$pk->playerEid = $p->getId();
+			$pk->displayName = "Trading";
+			$writer = new NetworkLittleEndianNBTStream();
+			$tags = new CompoundTag("Offers", [
+						new ListTag("Recipes", [
+						new CompoundTag("", [
+						new ByteTag("rewardExp", 1),
+						new IntTag("maxUses", mt_rand(2, 12)),
+						new IntTag("uses", 0),
+						Item::get(Item::DIAMOND)->nbtSerialize(-1, "buy"),
+						Item::get(Item::PAPER)->nbtSerialize(-1, "sell")
+			])
+			])
 			]);
-			$recipes = [[Item::get(Item::BRICK, 0, 1), Item::get(Item::SANDSTONE, 2, 2)], [Item::get(Item::BRICK, 0, 7), Item::get(Item::END_STONE, 0, 1)]];
-			$this->recipes[$p->getName()] = $recipes;
-			$i = 0;
-			foreach($recipes as $recipe){
-				$tag->Recipes[$i] = $this->makeRecipe($recipe[0], $recipe[1], $recipe[2] ?? null);
-				++$i;
-			}
-			$nbt = new NBT;
-			$nbt->setData($tag);
-
-			$tr = new UpdateTradePacket;
-			$tr->windowId = 2;
-			$tr->varint1 = 0;
-			$tr->varint2 = 0;
-			$tr->isWilling = false;
-			$tr->traderEid = $eid;
-			$tr->playerEid = -1;
-			$tr->displayName = "Shop";
-			$tr->offers = $nbt->write(true);
-			$p->dataPacket($tr);
+			$pk->offers = $writer->write($tags);
+			$p->dataPacket($pk);
 		}
+	}
+
+	public function Entity(): Entity{
+		return $this->en;
 	}
 
 	public function onDisconnect(PlayerQuitEvent $e){
 		$p = $e->getPlayer();
 		if(isset($this->villagerId[$p->getName()])){
 			unset($this->villagerId[$p->getName()]);
-			unset($this->recipes[$p->getName()]);
 		}
 	}
 
@@ -115,13 +96,11 @@ class Trading extends PluginBase implements Listener{
 				$pk->entityUniqueId = $eid = $this->villagerId[$player->getName()];
 				$player->dataPacket($pk);
 				unset($this->villagerId[$player->getName()]);
-				unset($this->recipes[$player->getName()]);
 			}
 		}
 		if($pk instanceof EntityEventPacket){
 			if($pk->event === 62){ //TRADING_TRANSACTION
-				if(isset($this->villagerId[$player->getName()]) and $pk->entityRuntimeId === $this->villagerId[$player->getName()] and isset($this->recipes[$player->getName()][$pk->data])){
-					$recipe = $this->recipes[$player->getName()][$pk->data];
+				if(isset($this->villagerId[$player->getName()]) and $pk->entityRuntimeId === $this->villagerId[$player->getName()] and isset($this->tags[$player->getName()][$pk->data])){
 					//TODO: make trading inventory
 					$e->setCancelled();
 				}
